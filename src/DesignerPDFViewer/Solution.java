4d1c5c28a49e73dee164ac048d55a032b44ac7f3
Darren O'Donnell <darren.odonnell001@gmail.com>
2021-04-20 23:13:12 +0100

    Designer PDF Viewer Problem Completed

---
+++ src/DesignerPDFViewer/Solution.java
@@ -0,0 +1,56 @@
+package DesignerPDFViewer;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the designerPdfViewer function below.
+    static int designerPdfViewer(int[] h, String word) {
+        //a = 97 in ASCII
+        int area = 0;
+        int highest = h[(int) word.charAt(0) - 97];
+        for(int i = 0; i < word.length(); i++){
+
+            int ch = (int) word.charAt(i) - 97;
+            if(h[ch] > highest){
+                highest = h[ch];
+            }
+        }
+
+        area = highest * word.length();
+        return area;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int[] h = new int[26];
+
+        String[] hItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 26; i++) {
+            int hItem = Integer.parseInt(hItems[i]);
+            h[i] = hItem;
+        }
+
+        String word = scanner.nextLine();
+
+        int result = designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
