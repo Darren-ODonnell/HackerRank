829ea2b76a5a5cc9e284352f87bd5206912cf4dd
Darren O'Donnell <darren.odonnell001@gmail.com>
2021-04-21 22:49:28 +0100

    DivisibleSumPairs Incomplete

---
+++ src/DivisibleSumPairs/Solution.java
@@ -0,0 +1,66 @@
+package DivisibleSumPairs;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the divisibleSumPairs function below.
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int sum = 0;
+        int pairs = 0;
+        for(int i = 0; i < ar.length; i++){
+            for(int x = 0; x < ar.length; x++){
+                if(i == 0 && x == 0){
+
+                }else if(i == n && x == n){
+                    
+                }else {
+                    sum = ar[i] + ar[x];
+
+                    if (sum % k == 0) {
+                        pairs++;
+                    }
+                }
+            }
+        }
+        return pairs/2;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = divisibleSumPairs(n, k, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
