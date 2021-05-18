1bb92472e92d9bd80f4d9343e5d8c71d2bf30d2f
Darren O'Donnell <darren.odonnell001@gmail.com>
2021-05-19 00:38:35 +0100

    Ice Cream Parlour- 3/4 tests passed

---
+++ src/IceCreamParlour/Solution.java
@@ -0,0 +1,86 @@
+package IceCreamParlour;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'icecreamParlor' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER m
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<Integer> icecreamParlor(int m, List<Integer> arr) {
+        // m = money
+        List<Integer> resultArray = new ArrayList<>();
+        for(int i = 0; i < arr.size(); i++){
+            for(int x = i; x < arr.size(); x++){
+                if(arr.get(i) != 0 && arr.get(x) != 0) {
+                    if (arr.get(i) + arr.get(x) == m) {
+
+                        if (!resultArray.contains(i + 1)) {
+                            resultArray.add(i + 1);
+                        }
+                        if (!resultArray.contains(x + 1)) {
+                            resultArray.add(x + 1);
+                        }
+                    }
+                }
+            }
+        }
+        while(resultArray.size()>2) {
+            resultArray.remove(resultArray.size()-1);
+        }
+        return resultArray;
+        // Write your code here
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int m = Integer.parseInt(bufferedReader.readLine().trim());
+
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList());
+
+                List<Integer> result = Result.icecreamParlor(m, arr);
+
+                bufferedWriter.write(
+                        result.stream()
+                                .map(Object::toString)
+                                .collect(joining(" "))
+                                + "\n"
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
