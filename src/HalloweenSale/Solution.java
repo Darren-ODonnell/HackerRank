a43c2dc928d971191db0f3e39282588dbf3812d8
Darren-ODonnell <darren.odonnell001@gmail.com>
2021-05-29 15:40:19 +0100

    Halloween Sale Complete

---
+++ src/HalloweenSale/Solution.java
@@ -0,0 +1,77 @@
+package HalloweenSale;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'howManyGames' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER p
+     *  2. INTEGER d
+     *  3. INTEGER m
+     *  4. INTEGER s
+     */
+
+    public static int howManyGames(int price, int drop, int lowestPrice, int spendingMoney) {
+        // Return the number of games you can buy
+
+        // price = start price
+        // d = how much it drops by each time
+        // m = lowest price it drops to
+        // s = money buyer has
+
+        int count = 0;
+
+        while(spendingMoney >= price){
+
+            spendingMoney -= price;
+            if(price - drop <= lowestPrice) {
+                price = lowestPrice;
+            }else{
+                price-=drop;
+            }
+            count++;
+        }
+        return count;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int p = Integer.parseInt(firstMultipleInput[0]);
+
+        int d = Integer.parseInt(firstMultipleInput[1]);
+
+        int m = Integer.parseInt(firstMultipleInput[2]);
+
+        int s = Integer.parseInt(firstMultipleInput[3]);
+
+        int answer = Result.howManyGames(p, d, m, s);
+
+        bufferedWriter.write(String.valueOf(answer));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
