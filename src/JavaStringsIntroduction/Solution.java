36ecd50dbb921b91c874bd55d710c0808eb0c2fa
Darren O'Donnell <darren.odonnell001@gmail.com>
2021-06-17 23:31:41 +0100

    Java Strings Introduction Complete

--- src/JavaStringsIntroduction/Solution.java
+++ src/JavaStringsIntroduction/Solution.java
@@ -1,3 +1,4 @@
+package JavaStringsIntroduction;
 import java.io.*;
 import java.util.*;
 
@@ -9,6 +10,18 @@ public class Solution {
         String A=sc.next();
         String B=sc.next();
         /* Enter your code here. Print output to STDOUT. */
+
+        int greater = A.compareTo(B);
+        System.out.println(A.length() + B.length());
+
+        if(greater <= 0){
+            System.out.println("No");
+        }else{
+            System.out.println("Yes");
+        }
+
+        System.out.print(A.substring(0,1).toUpperCase() + A.substring(1) + " ");
+        System.out.print(B.substring(0,1).toUpperCase() + B.substring(1));
 
     }
 }
